5a1abcf04143b22d8a2b172a2a449eba3349e6d8
JJ Smiley <github@grandmasterj.com>
2026-07-16 15:36:24 -0400

    Aesthetic fixes: remove extra period and grid background from src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" suppressHydrationWarning>
       <body className={`${body.variable} ${display.variable} ${mono.variable} font-sans`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          <div className="relative flex min-h-screen flex-col bg-grid-pattern bg-[length:40px_40px]">
+          <div className="relative flex min-h-screen flex-col bg-black">
             <Navbar />
             <main className="flex-1">{children}</main>
             <Footer />
